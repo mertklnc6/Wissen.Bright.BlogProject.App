604163f0a543ed3b38723014249409afe3793f0e
mertklnc6 <120830925+mertklnc6@users.noreply.github.com>
2023-09-24 19:21:48 +0300

    Add project files.

---
+++ Wissen.Bright.BlogProject.App.Service/Services/AccountService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wissen.Bright.BlogProject.App.DataAccess.Identity;
+using Wissen.Bright.BlogProject.App.Entity.Services;
+using Wissen.Bright.BlogProject.App.Entity.ViewModels;
+
+namespace Wissen.Bright.BlogProject.App.Service.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountService(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public async Task<string> CreateUserAsync(RegisterViewModel model)
+        {
+            string message = string.Empty;
+            var user = new AppUser()
+            {
+                Name = model.FirstName,
+                Surname = model.LastName,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Username
+            };
+            var identityResult = await _userManager.CreateAsync(user, model.ConfirmPassword);
+
+            if (identityResult.Succeeded)
+            {
+                message = "OK";
+            }
+            else
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    message = error.Description;
+                }
+            }
+            return message;
+        }
+        public async Task<string> FindByNameAsync(LoginViewModel model)
+        {
+            string message = string.Empty;
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if(user == null)
+            {
+                message = "user not found";
+                return message;
+            }
+            var signInResult = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);  
+            if (signInResult.Succeeded)
+            {
+                message = "OK";
+            }
+            return message;
+        }
+
+		public async Task LogoutAsync()
+		{
+			await _signInManager.SignOutAsync();
+		}
+	}
+}
